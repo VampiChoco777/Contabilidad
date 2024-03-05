8895f44eff9fa183966ee2bbf0f1f99921111959
Marco Adrián Domínguez Jiménez <l20140892@queretaro.tecnm.mx>
2024-03-04 20:25:49 -0600

    first commit

--- src/app/components/checker/checker.component.ts
+++ src/app/components/checker/checker.component.ts
@@ -6,18 +6,71 @@ import { Component } from '@angular/core';
   styleUrl: './checker.component.scss'
 })
 export class CheckerComponent {
+  /*
   passive: number = 0;
   active: number = 0;
   capital: number = 0;
   /*active: number | null = 0;*/
 
+  /*
+
+
+
   activeValues: number[] = [];
   passiveValues: number[] = [];
-  capitalValues: number[] = [];
+  capitalValues: number[] = [];*/
 
-  constructor() { }
+  activeValue: number = 0;
+  activeDescription: string = '';
 
+  passiveValue: number = 0;
+  passiveDescription: string = '';
 
+  capitalValue: number = 0;
+  capitalDescription: string = '';
+
+  activeValues: {description: string, value: number}[] = [];
+  passiveValues: {description: string, value: number}[] = [];
+  capitalValues: {description: string, value: number}[] = [];
+
+  constructor() {}
+
+  addActive() {
+    if (!isNaN(this.activeValue)) {
+      this.activeValues.push({description: String(this.activeDescription), value: Number(this.activeValue)});
+      this.activeValue = 0;
+      this.activeDescription = '';
+    }
+  }
+
+  addPassive() {
+    if (!isNaN(this.passiveValue)) {
+      this.passiveValues.push({description: String(this.passiveDescription), value: Number(this.passiveValue)});
+      this.passiveValue = 0;
+      this.passiveDescription = '';
+    }
+  }
+
+  addCapital() {
+    if (!isNaN(this.capitalValue)) {
+      this.capitalValues.push({description: String(this.capitalDescription), value: Number(this.capitalValue)});
+      this.capitalValue = 0;
+      this.capitalDescription = '';
+    }
+  }
+
+  isBalanceCorrect(): boolean {
+    return this.checkBalance();
+  }
+
+  private checkBalance(): boolean {
+    const totalActive = this.activeValues.reduce((total, item) => total + item.value, 0);
+    const totalPassive = this.passiveValues.reduce((total, item) => total + item.value, 0);
+    const totalCapital = this.capitalValues.reduce((total, item) => total + item.value, 0);
+    return totalActive + totalPassive === totalCapital;
+  }
+
+  /*
   addPassive(): void {
     this.passiveValues.push(Number(this.passive)); // Convierte el valor a número
     this.passive = 0;
@@ -42,5 +95,5 @@ export class CheckerComponent {
     const totalPassive = this.passiveValues.reduce((a, b) => a + b, 0);
     const totalCapital = this.capitalValues.reduce((a, b) => a + b, 0);
     return totalActive + totalPassive === totalCapital;
-  }
+  }*/
 }
